effd212beb218688e3c59ac00dd4578fa24bf2e0
abhipatil119 <patil.abhis@nortehastern.edu>
2024-11-06 21:03:19 -0500

    scheduling

---
+++ Code_Development/pythonscheduler.bat
@@ -0,0 +1,4 @@
+@echo off
+C:\Users\amey8\AppData\Local\Microsoft\WindowsApps\python.exe F:\AdvDMDD\FinalProject\P4\malnurtition_refresh_sql.py
+C:\Users\amey8\AppData\Local\Microsoft\WindowsApps\python.exe F:\AdvDMDD\FinalProject\P4\gremlineapi_refresh.py
+pause
